fec3cf6421309620079eff02adc13883a8ac020b
nilesh <manjaramkarnil@gmail.com>
2024-04-07 10:56:31 +0530

    ng

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'project3';
+  skills : Array<string> = ["html","css","ts","js"]
+
+}
